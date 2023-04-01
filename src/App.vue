2c75a9845bad280581ee16698443e1a807ab7d0f
langyamu <1764714737@qq.com>
2023-04-01 17:37:31 +0800

    :tada:monaco-editor 在 vite + vue3 + ts 下的基本示例，非全量引入模式

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,57 @@
 <script setup lang="ts">
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import { nextTick, onMounted, onUnmounted, ref } from "vue";
+// 注册 TypeScript 语言的语法解释器，提供语法高亮等，是 TypeScript 语言的基础能力
+import "monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution";
+// @ts-ignore
+self.MonacoEnvironment = {
+  getWorker(_: string, label: string) {
+    if (label === "typescript" || label === "javascript") {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+
+const editor_dom = ref<HTMLElement>();
+let editorIns: monaco.editor.IStandaloneCodeEditor;
+
+onMounted(async () => {
+  await nextTick();
+
+  editorIns = monaco.editor.create(
+    editor_dom.value as HTMLElement,
+    {
+      value: "function(){}",
+      language: "typescript",
+      theme: "vs",
+      readOnly: false,
+    }
+  );
+
+  // editorIns.getAction("editor.action.formatDocument")?.run()
+
+  editorIns.onDidChangeModelContent(function (e) {
+    editorIns?.layout();
+  });
+
+  onUnmounted(() => {
+    //  ( editor_ins.value as monaco.editor.IStandaloneCodeEditor).dispose();  // 销毁
+  });
+});
 </script>
 
 <template>
-  <h1>
-    hello world!!
-  </h1>
+  <section class="editor-container">
+    <div class="editor" ref="editor_dom"></div>
+  </section>
 </template>
 
 <style scoped>
- 
+.editor {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
